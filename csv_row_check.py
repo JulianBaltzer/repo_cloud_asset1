13c41a2e14d65bc130c1b18c46f18f5a3b798541
LCyclix <jbaltzer21@gmail.com>
2022-08-29 08:11:38 +0200

    Freigabe für tests

--- csv_row_check.py
+++ csv_row_check.py
@@ -1,10 +1,8 @@
 from cgi import test
-import csv
 from hashlib import new
 from multiprocessing import connection
 import shutil
 import os
-import csv
 import pandas as pd
 import glob
 import datetime
@@ -28,6 +26,7 @@ destination = "C:/Users/Michael.Malkmus/OneDrive - FUNKE Mediengruppe/Desktop/Cl
 #Errorverzeichnis
 errorverzeichnis = "C:/Users/Michael.Malkmus/OneDrive - FUNKE Mediengruppe/Desktop/Cloud Assets Projekt/csv_import/errorverzeichnis/"
 
+done = "Pfad hier einfügen"
 #Datei in Quellverzeichnis vorhanden?
 
 if len(os.listdir('C:/Users/Michael.Malkmus/OneDrive - FUNKE Mediengruppe/Desktop/Cloud Assets Projekt/csv_import/quellverzeichnis') ) == 0:
@@ -61,53 +60,33 @@ for filename in filenames:
         
         tags_df = dataframe.filter(regex=r'^tag')
 
+        if dataframe.empty:
+                    print("Keine Datensätz zu verarbeiten")
+                    sys.exit(1)
+
         dataframe = dataframe[dataframe.columns.drop(list(dataframe.filter(regex=r'^tag')))]
         rows = dataframe.values.tolist()
 
-        if dataframe.empty:
-            print("Keine Datensätz zu verarbeiten")
-            sys.exit(1)
+        
 
         cursor.execute("Insert into q_status VALUES(0,'Nicht verarbeitet')")
         cursor.execute("Insert into q_status VALUES(1,'Verarbeitet')")
 
         for x in rows:
-            cursor.execute("Insert into q VALUES({dbindate},{dbinuser},{dbupdateuser},{message},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{})".format(rows[x][13],
-                                                                                                                                                        rows[x][14],
-                                                                                                                                                        rows[x][15],
-                                                                                                                                                        rows[x][16],
-                                                                                                                                                        rows[x][17],
-                                                                                                                                                        rows[x][18],
-                                                                                                                                                        rows[x][19],
-                                                                                                                                                        rows[x][0],
-                                                                                                                                                        rows[x][1],
-                                                                                                                                                        rows[x][2],
-                                                                                                                                                        rows[x][3],
-                                                                                                                                                        rows[x][4],
-                                                                                                                                                        rows[x][5],
-                                                                                                                                                        rows[x][6],
-                                                                                                                                                        rows[x][7],
-                                                                                                                                                        rows[x][8],
-                                                                                                                                                        rows[x][9],
-                                                                                                                                                        rows[x][10],
-                                                                                                                                                        rows[x][11],
-                                                                                                                                                        rows[x][12]))
+            cursor.execute("Insert into q VALUES({dbindate},{dbinuser},{dbupdateuser},{message},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{})".format(rows[x][13],rows[x][14],rows[x][15],rows[x][16],rows[x][17],rows[x][18],rows[x][19],rows[x][0],rows[x][1],rows[x][2],rows[x][3],rows[x][4],rows[x][5],rows[x][6],rows[x][7],rows[x][8],rows[x][9],rows[x][10],rows[x][11],rows[x][12]))
             
-        
+        shutil.move(destination+ filename, done + filename)  
     except:
         print("Error in" + filename)
         # Errorverzeichnis verschieben 
-        filename  = "reports_cost-csv_0001000000644485.csv"
         now = datetime.datetime.now()
         new_name = now.strftime("%d_%m_%Y %H_%M_%S") + filename
         os.rename(destination+filename, destination+ new_name)
         shutil.move(destination+ new_name, errorverzeichnis+ new_name)
 
     
+    #TO DOS 
 # Tags tabelle insert 
-
-
-# Okay verzeichnis verschieben an richtiger stelle
 # praxitest
 
 
